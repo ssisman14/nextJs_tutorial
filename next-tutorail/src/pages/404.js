90445912f74dfc498625b80d5752712a4aceeb41
selahattin_sisman <selahattin_sisman@emrisoft.com>
2023-09-15 10:33:59 +0300

    first

---
+++ next-tutorail/src/pages/404.js
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <>
+      <h3>404 | This page colud not be found.</h3>
+    </>
+  );
+};
+
+export default NotFoundPage;
---
+++ next-tutorail/src/pages/events/index.js
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../../dummy-data";
+import EventList from "@/components/events/eventList";
+import EventsSearch from "@/components/events/event-search";
+
+function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export default AllEventsPage;
